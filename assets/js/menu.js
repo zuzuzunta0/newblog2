2a3865f1c1eb5b27aa67f6c8668f0925df0e651f
zuzuzunta0 <kohsub469@gmail.com>
2022-02-26 15:15:46 +0900

    save

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,18 +1,20 @@
 // Mobile menu
 
-const menuTrigger = document.querySelector(".menu-trigger");
-const menu = document.querySelector(".menu");
+const menuTrigger = document.querySelector(".menu-trigger"); //.menu-triggerというクラスを読み込む
+const menu = document.querySelector(".menu");  //.menuというクラスを読み込む
 const mobileQuery = getComputedStyle(document.body).getPropertyValue(
   "--phoneWidth"
-);
-const isMobile = () => window.matchMedia(mobileQuery).matches;
+); //phoneWidthの値を読み込む
+const isMobile = () => window.matchMedia(mobileQuery).matches; //モバイルのウィンドウサイズとマッチした時
 const isMobileMenu = () => {
-  menuTrigger && menuTrigger.classList.toggle("hidden", !isMobile());
-  menu && menu.classList.toggle("hidden", isMobile());
+  menuTrigger && menuTrigger.classList.toggle("hidden", !isMobile()); //ハンバーガーボタンの表示非表示きりかえ
+  menu && menu.classList.toggle("hidden", isMobile()); //PC画面で表示される横並びのヘッダーの表示非表示切り替え
 };
 
 isMobileMenu();
 
+
+//クリックで表示非表示切り替え
 menuTrigger &&
   menuTrigger.addEventListener(
     "click",
